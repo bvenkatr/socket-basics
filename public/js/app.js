a9f68f903da070429dc0a2e465eab2b32f614979
Naveen Pete <naveen.shenoy.blr@gmail.com>
2016-03-23 16:13:49 +0530

    Move getQueryParams() function temporarily to app.js

--- public/js/app.js
+++ public/js/app.js
@@ -35,3 +35,17 @@ $form.on('submit', function (event) {
     
     $message.val('');
 });
+
+// Get query string variable value
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    
+    return undefined;
+}
